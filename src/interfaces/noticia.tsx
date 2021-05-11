d5d4e728f21ee6cb29d679b3ec15b90b1cbbacfb
Jandson <jandsoncavalcanti@live.com>
2021-05-11 16:10:29 -0300

    adicao de interface de noticia

---
+++ src/interfaces/noticia.tsx
@@ -0,0 +1,13 @@
+import { Usuario } from "./usuario";
+
+export interface Noticia {
+    id: number;
+    titulo: string;
+    corpo: string;
+    publicacao: Date;
+    autor: Usuario;
+    quantidadeDeAcessos: number;
+    ativo: boolean;
+    destaque: boolean;
+    ordem: number;
+}
