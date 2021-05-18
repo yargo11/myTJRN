17dedbd54540d5b22a45de811a8b8aef87b7ae6c
Jandson <jandsoncavalcanti@live.com>
2021-05-18 18:05:55 -0300

    adicionando testes com mirage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { SearchBanner } from '../components/HomeComponents/SearchBanner/index'
 import { HomeNews } from '../components/HomeComponents/HomeNews/index'
 import { ServicesAndInformations } from '../components/HomeComponents/ServicesAndInformations/index'
+import { createApi } from '../../test/api.test'
 
 export default function Home() {
+  createApi();
   return (
     <>
       <SearchBanner />
