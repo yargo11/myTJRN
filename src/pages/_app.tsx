0efd170743b0ca9a4c9a98d73af00386a99b3485
Yargo Valério dos Santos Abou Ali <f202367@intrajus.tjrn>
2021-06-11 13:04:43 -0300

    instalando chrakra-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,19 @@
-import '../styles/global.scss';
-
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
 
+import '../styles/global.scss';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../styles/theme';
+
+
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ChakraProvider theme={theme}>
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </ChakraProvider>
   )
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+    fonts: {
+        heading: 'Raleway',
+        body: 'Raleway'
+    },
+    styles: {
+        global: {
+            body: {
+                bg: 'white',
+                color: 'gray.900'
+            }
+        }
+    }
+})
