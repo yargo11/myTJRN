419e2f08e5d35642df7e475414b05c344c661564
Jandson <jandsoncavalcanti@live.com>
2021-06-14 16:51:15 -0300

    iniciando a inserir o chakra ui

---
+++ src/styles/theme.ts
@@ -0,0 +1,30 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+    colors: {
+        white: '#fff',
+        gray: {
+            '100': '#e1e1e6',
+            '200': '#cbcbcf',
+            '800': '#29292e',
+            '900': '#121214'
+        },
+        cyan: {
+            '500': '#61C5EC'
+        },
+        yellow: {
+            '500': '#eba417'
+        }
+    },
+    fonts: {
+        heading: 'Inter, sans-serif',
+        body: 'Inter, sans-serif'
+    },
+    styles: {
+        global: {
+            button: {
+                cursor: 'pointer'
+            }
+        }
+    }
+});
