0e5f35035c01ac2226473fbffb4e5835a485107e
jandson <jandsoncavalcanti@live.com>
2021-09-24 12:43:55 -0300

    correção de identação

--- src/components/Footer/PolicyFuncionalityList/index.tsx
+++ src/components/Footer/PolicyFuncionalityList/index.tsx
@@ -3,18 +3,18 @@ import { UnorderedList, ListItem, Link } from '@chakra-ui/react';
 export default function PolicyFuncionalityList () {
     return (
         <UnorderedList
-        display='flex'
-        flexWrap='wrap'
-        listStyleType='none'
-        color='#F2F2F2'
-        fontFamily='Inter'
-        fontWeight='normal'
-        lineHeight='1.21rem'
-        mt='1rem'
-        mx='0'>
-        <ListItem as={Link} href='#'>Termos de uso</ListItem>
-        <ListItem as={Link} href='#' ml='1rem'>Políticas de privacidade</ListItem>
-        <ListItem as={Link} href='#' ml='1rem'>Mapa do site</ListItem>
+            display='flex'
+            flexWrap='wrap'
+            listStyleType='none'
+            color='#F2F2F2'
+            fontFamily='Inter'
+            fontWeight='normal'
+            lineHeight='1.21rem'
+            mt='1rem'
+            mx='0'>
+            <ListItem as={Link} href='#'>Termos de uso</ListItem>
+            <ListItem as={Link} href='#' ml='1rem'>Políticas de privacidade</ListItem>
+            <ListItem as={Link} href='#' ml='1rem'>Mapa do site</ListItem>
     </UnorderedList>
     );
 }
